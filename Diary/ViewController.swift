4cb6d895a43e1ec9f0553f0eb8f774b2acda6e75
Bharath Chandrashekar <bharath.chandrashekar@fluke.com>
2019-09-10 23:46:31 +0530

    Initial commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Bharath on 10/09/19.
+//  Copyright © 2019 Bharath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
